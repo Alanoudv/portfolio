d17cc75a538d65c5c6f000873e0d83b222d90835
Alanoud <alanoodabduallah110@gmail.com>
2024-04-27 01:10:54 +0300

    Website

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+    var typed = new typed(".mltiple-text", {
+        strings: ["FullStack Developer" , "API Designer" , "Software Tester"] ,
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
